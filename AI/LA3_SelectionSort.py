6aee06561eae048f312eca11e27471303033ad75
Sahil Mate <143534628+sahilmate@users.noreply.github.com>
2025-02-27 00:36:01 +0530

    Create LA3_SelectionSort.py

---
+++ AI/LA3_SelectionSort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # Swap
+
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+selection_sort(numbers)
+print("Sorted numbers:", numbers)
